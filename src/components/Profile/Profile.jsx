935411bca30d5bf2a2f5194509e5d416d2eae472
Feyza Yarcı <yarcifeyza@gmail.com>
2025-03-12 16:16:33 +0300

    edited

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -28,10 +28,10 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
 };
 
 Profile.propTypes = {
-  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
   stats: PropTypes.shape({
     followers: PropTypes.number.isRequired,
     views: PropTypes.number.isRequired,
